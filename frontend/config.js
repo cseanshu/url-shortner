2e5f8c41a8d94ef575548ba24456a2eba0c5c019
anshumanmishra <mishraanshuman1008@gmail.com>
2025-11-19 22:13:16 +0530

    updated the frontend with the deployed url

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const config={
-VITE_API_URL:'http://localhost:8080',
+VITE_API_URL:'https://url-shortner-9h7j.onrender.com',  //'http://localhost:8080',
 FRONTEND_URL:'http://localhost:5173',
 PORT:8080
 }

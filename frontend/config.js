31da77446d0bd27efc3573239ab4bc03f83eca71
anshumanmishra <mishraanshuman1008@gmail.com>
2025-11-19 22:27:41 +0530

    correct the server_url

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,5 @@
 const config={
-VITE_API_URL:'https://url-shortner-9h7j.onrender.com',  //'http://localhost:8080',
-FRONTEND_URL:'http://localhost:5173',
+SERVER_API_URL:'https://url-shortner-9h7j.onrender.com',  //'http://localhost:8080',
 PORT:8080
 }
 export default config
